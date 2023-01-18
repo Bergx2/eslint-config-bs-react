72678b87bb0f313129c25fa7ece15c88fc169ce6
Rybalka Denis <denis@bergx2.de>
2023-01-18 17:39:29 +0200

    Set up eslint plugins for react

---
+++ rules/react.js
@@ -0,0 +1,25 @@
+module.exports = {
+  plugins: ['react-hooks'],
+  extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  rules: {
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+    'react-hooks/exhaustive-deps': 0,
+    'react/no-danger': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-closing-tag-location': 0,
+    'react/jsx-fragments': 0,
+  },
+  settings: {
+    react: {
+      createClass: 'createReactClass',
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+};
